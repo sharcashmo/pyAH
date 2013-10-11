9e4048b527043c621f509b101fdfbb83f6bc4fbf
David Lozano <david.lozano.ruiz@gmail.com>
2013-10-12 00:01:08 +0200

    New unit tests for atlantis.helpers.comparable module.

---
+++ pyAH/atlantis/helpers/tests/test_comparable.py
@@ -0,0 +1,26 @@
+"""Unit tests for :mod:`atlantis.helpers.json`."""
+
+from atlantis.helpers.comparable import RichComparable
+
+import unittest
+
+
+class TestRichComparable(unittest.TestCase):
+    """Test :class:`~atlantis.helpers.comparable.RichComparable`."""
+
+    def test_comparable(self):
+        """Test :class:`TestRichComparable` added functionality."""
+        
+        class A(RichComparable):
+            def __init__(self, a, b, c):
+                self.a = a
+                self.b = b
+                self.c = c
+        
+        o1 = A(2, [5, 8, 12], 'pedro')
+        o2 = A(2, [5, 8, 12], 'pedro')
+        
+        self.assertEqual(o1, o2)
+
+if __name__ == '__main__':
+    unittest.main()
