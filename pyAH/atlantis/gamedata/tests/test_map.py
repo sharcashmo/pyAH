55bc6b14419e524c772ed8017c15a3575063d42d
David Lozano <david.lozano.ruiz@gmail.com>
2013-10-31 00:29:49 +0100

    New. Unit tests for atlantis.gamedata.region module.

---
+++ pyAH/atlantis/gamedata/tests/test_map.py
@@ -0,0 +1,231 @@
+"""Unit tests for :mod:`atlantis.gamedata.map`."""
+
+from atlantis.gamedata.map import MapHex, MapLevel, Map, \
+    HEX_CURRENT, HEX_OLD, HEX_EXITS, SEEN_CURRENT
+from atlantis.gamedata.region import Region
+
+from io import StringIO
+
+import json
+import unittest
+
+class TestMapHex(unittest.TestCase):
+    """Test :class:`atlantis.gamedata.map.MapHex`."""
+    
+    def test_constructor(self):
+        """Test :class:`~atlantis.gamedata.map.MapHex` constructor."""
+        
+        r = Region((21, 93, None), 'plain', 'Isshire', 9836, 'vikings', 11016,
+                   {'name': 'Durshire', 'type': 'town'})
+        mh = MapHex(r, HEX_CURRENT)
+        self.assertEqual(mh.region, r)
+        self.assertEqual(mh.status, HEX_CURRENT)
+        self.assertEqual(mh.last_seen, SEEN_CURRENT)
+        
+        
+        r = Region((21, 93, None), 'plain', 'Isshire',
+                   town={'name': 'Durshire', 'type': 'town'})
+        mh = MapHex(r, HEX_EXITS, (1, 12))
+        self.assertEqual(mh.region, r)
+        self.assertEqual(mh.status, HEX_EXITS)
+        self.assertEqual(mh.last_seen, (1, 12))
+    
+    def test_get_region_info(self):
+        """Test :meth:`~atlantis.gamedata.map.MapHex.get_region_info`."""
+        r = Region((21, 93, None), 'plain', 'Isshire', 9836, 'vikings', 11016,
+                   {'name': 'Durshire', 'type': 'town'})
+        mh = MapHex(r, HEX_CURRENT)
+        
+        r2 = mh.get_region_info()
+        self.assertEqual(r, r2)
+    
+    def test_get_last_seen(self):
+        """Test :meth:`~atlantis.gamedata.map.MapHex.get_last_seen`."""
+        r = Region((21, 93, None), 'plain', 'Isshire', 9836, 'vikings', 11016,
+                   {'name': 'Durshire', 'type': 'town'})
+        mh = MapHex(r, HEX_OLD, (1, 12))
+        
+        self.assertEqual(mh.get_last_seen(), (1, 12))
+    
+    def test_is_current(self):
+        """Test :meth:`~atlantis.gamedata.map.MapHex.is_current`."""
+        r = Region((21, 93, None), 'plain', 'Isshire', 9836, 'vikings', 11016,
+                   {'name': 'Durshire', 'type': 'town'})
+        
+        mh = MapHex(r, HEX_OLD, (1, 12))
+        self.assertFalse(mh.is_current())
+        
+        mh = MapHex(r, HEX_CURRENT)
+        self.assertTrue(mh.is_current())
+        
+        mh = MapHex(r, HEX_EXITS)
+        self.assertFalse(mh.is_current())
+    
+    def test_is_complete(self):
+        """Test :meth:`~atlantis.gamedata.map.MapHex.is_complete`."""
+        r = Region((21, 93, None), 'plain', 'Isshire', 9836, 'vikings', 11016,
+                   {'name': 'Durshire', 'type': 'town'})
+        
+        mh = MapHex(r, HEX_OLD, (1, 12))
+        self.assertTrue(mh.is_complete())
+        
+        mh = MapHex(r, HEX_CURRENT)
+        self.assertTrue(mh.is_complete())
+        
+        mh = MapHex(r, HEX_EXITS)
+        self.assertFalse(mh.is_complete())
+        
+
+    def test_json_methods(self):
+        """Test implementation of 
+        :meth:`~atlantis.helpers.json.JsonSerializeble` interface."""
+        io = StringIO()
+        
+        r = Region((21, 93, None), 'plain', 'Isshire', 9836, 'vikings', 11016,
+                   {'name': 'Durshire', 'type': 'town'})
+        mh = MapHex(r, HEX_CURRENT)
+        json.dump(mh, io, default=MapHex.json_serialize)
+        io.seek(0)
+        mh_new = MapHex.json_deserialize(json.load(io))
+        
+        self.assertEqual(mh, mh_new)
+
+class TestMapLevel(unittest.TestCase):
+    """Test :class:`~atlantis.gamedata.game.MapLevel`."""
+    
+    def test_contructor(self):
+        """Test :class:`~atlantis.gamedata.game.MapLevel` constructor."""
+        
+        lvl = MapLevel('surface')
+        self.assertEqual(lvl.name, 'surface')
+        self.assertEqual(lvl.hexes, dict())
+        
+    def test_set_region(self):
+        """Test :meth:`~atlantis.gamedata.game.MapLevel.set_region`."""
+        
+        lvl = MapLevel('surface')
+        self.assertEqual(lvl.name, 'surface')
+        self.assertEqual(lvl.hexes, dict())
+        
+        r = Region((21, 93, None), 'plain', 'Isshire', 9836, 'vikings', 11016,
+                   {'name': 'Durshire', 'type': 'town'})
+        mh = MapHex(r, HEX_CURRENT)
+        lvl.set_region(mh)
+        self.assertEqual(lvl.hexes, {(21, 93): mh})
+        
+        r = Region((21, 93, 'underworld'), 'plain', 'Isshire', 9836, 'vikings',
+                   11016, {'name': 'Durshire', 'type': 'town'})
+        mh = MapHex(r, HEX_CURRENT)
+        self.assertRaises(KeyError, lvl.set_region, mh)
+    
+    def test_get_region(self):
+        """Test :meth:`~atlantis.gamedata.game.MapLevel.get_region`."""
+        
+        lvl = MapLevel('surface')
+        self.assertEqual(lvl.name, 'surface')
+        self.assertEqual(lvl.hexes, dict())
+        
+        r = Region((21, 93, None), 'plain', 'Isshire', 9836, 'vikings', 11016,
+                   {'name': 'Durshire', 'type': 'town'})
+        mh = MapHex(r, HEX_CURRENT)
+        lvl.hexes[(21,93)] = mh
+        
+        mh_new = lvl.get_region((21,93))
+        self.assertIs(mh, mh_new)
+        
+        self.assertIsNone(lvl.get_region((1,1)))
+
+    def test_json_methods(self):
+        """Test implementation of 
+        :meth:`~atlantis.helpers.json.JsonSerializeble` interface."""
+        io = StringIO()
+        
+        lvl = MapLevel('surface')
+        r = Region((21, 93, None), 'plain', 'Isshire', 9836, 'vikings', 11016,
+                   {'name': 'Durshire', 'type': 'town'})
+        mh = MapHex(r, HEX_CURRENT)
+        lvl.set_region(mh)
+        
+        json.dump(lvl, io, default=MapLevel.json_serialize)
+        io.seek(0)
+        lvl_new = MapLevel.json_deserialize(json.load(io))
+        
+        self.assertEqual(lvl, lvl_new)
+
+
+class TestMap(unittest.TestCase):
+    """Test :class:`~atlantis.gamedata.game.Map`."""
+    
+    def test_add_region_info(self):
+        """Test :meth:`~atlantis.gamedata.game.Map.add_region_info`."""
+        
+        m = Map()
+        r = Region((21, 93, None), 'plain', 'Isshire', 9836, 'vikings', 11016,
+                   {'name': 'Durshire', 'type': 'town'})
+        m.add_region_info(r)
+        
+        self.assertEqual(list(m.levels.keys()), ['surface'])
+        lvl = m.levels['surface']
+        self.assertEqual(list(lvl.hexes.keys()), [(21, 93)])
+        mh = lvl.hexes[(21, 93)]
+        self.assertEqual(mh.region, r)
+        self.assertEqual(mh.status, HEX_CURRENT)
+        self.assertEqual(mh.last_seen, SEEN_CURRENT)
+    
+        r2 = Region((21, 93, None), 'plain', 'Isshire',
+                    town={'name': 'Durshire', 'type': 'town'})
+        m.add_region_info(r2, HEX_EXITS)
+        
+        self.assertEqual(list(m.levels.keys()), ['surface'])
+        lvl = m.levels['surface']
+        self.assertEqual(list(lvl.hexes.keys()), [(21, 93)])
+        mh = lvl.hexes[(21, 93)]
+        self.assertEqual(mh.region, r)
+        self.assertEqual(mh.status, HEX_CURRENT)
+        self.assertEqual(mh.last_seen, SEEN_CURRENT)
+        
+        mh.status = HEX_OLD
+        mh.last_seen = (2, 1)
+        r3 = Region((21, 93, None), 'plain', 'Isshire', 9856, 'vikings', 11116,
+                    {'name': 'Durshire', 'type': 'town'})
+        m.add_region_info(r3, HEX_CURRENT)
+        
+        self.assertEqual(list(m.levels.keys()), ['surface'])
+        lvl = m.levels['surface']
+        self.assertEqual(list(lvl.hexes.keys()), [(21, 93)])
+        mh = lvl.hexes[(21, 93)]
+        self.assertEqual(mh.region, r3)
+        self.assertEqual(mh.status, HEX_CURRENT)
+        self.assertEqual(mh.last_seen, SEEN_CURRENT)
+    
+    def test_get_region(self):
+        """Test :class:`~atlantis.gamedata.game.Map` constructor."""
+        m = Map()
+        r = Region((21, 93, None), 'plain', 'Isshire', 9836, 'vikings', 11016,
+                   {'name': 'Durshire', 'type': 'town'})
+        m.add_region_info(r)
+        
+        mh = m.get_region((21, 93, 'surface'))
+        self.assertEqual(mh.region, r)
+        
+        mh = m.get_region((21, 93, None))
+        self.assertEqual(mh.region, r)
+
+    def test_json_methods(self):
+        """Test implementation of 
+        :meth:`~atlantis.helpers.json.JsonSerializeble` interface."""
+        io = StringIO()
+         
+        m = Map()
+        r = Region((21, 93, None), 'plain', 'Isshire', 9836, 'vikings', 11016,
+                   {'name': 'Durshire', 'type': 'town'})
+        m.add_region_info(r)
+         
+        json.dump(m, io, default=Map.json_serialize)
+        io.seek(0)
+        m_new = Map.json_deserialize(json.load(io))
+         
+        self.assertEqual(m, m_new)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ pyAH/atlantis/gamedata/tests/test_region.py
@@ -0,0 +1,187 @@
+"""Unit tests for :mod:`atlantis.gamedata.region`."""
+
+from atlantis.gamedata.region import Region
+from atlantis.gamedata.item import ItemAmount, ItemMarket
+
+from io import StringIO
+
+import json
+import unittest
+
+class TestRegion(unittest.TestCase):
+    """Test class :class:`atlantis.gamedata.region.Region`."""
+    
+    def test_constructor(self):
+        """Test :class:`~atlantis.gamedata.region.Region` constructor."""
+        region = Region((21, 93, None), 'plain', 'Isshire',
+                        2392, 'vikings', 11016,
+                        {'name': 'Durshire', 'type': 'town'})
+        self.assertEqual(region.location, (21, 93, None))
+        self.assertEqual(region.terrain, 'plain')
+        self.assertEqual(region.name, 'Isshire')
+        self.assertEqual(region.population, 2392)
+        self.assertEqual(region.racenames, 'vikings')
+        self.assertEqual(region.wealth, 11016)
+        self.assertEqual(region.town, {'name': 'Durshire', 'type': 'town'})
+        
+        region = Region((21,93, None), 'plain', 'Isshire',
+                        town={'name': 'Durshire', 'type': 'town'})
+        self.assertEqual(region.location, (21, 93, None))
+        self.assertEqual(region.terrain, 'plain')
+        self.assertEqual(region.name, 'Isshire')
+        self.assertEqual(region.population, 0)
+        self.assertIsNone(region.racenames)
+        self.assertEqual(region.wealth, 0)
+        self.assertEqual(region.town, {'name': 'Durshire', 'type': 'town'})
+    
+    def test_append_report_description(self):
+        """Test
+        :meth:`~atlantis.gamedata.region.Region.append_report_description`.
+        """
+        region = Region((21, 93, None), 'plain', 'Isshire',
+                        2392, 'vikings', 11016,
+                        {'name': 'Durshire', 'type': 'town'})
+        
+        lines = ['plain (21,93) in Isshire, contains Durshire [town], 9836 '
+                 'peasants (vikings), $11016.',
+                 '------------------------------------------------------------',
+                 '  The weather was clear last month; it will be clear next '
+                 'month.',
+                 '  Wages: $15.6 (Max: $3672).',
+                 '  Wanted: 171 grain [GRAI] at $24, 109 livestock [LIVE] at '
+                 '$24, 139 fish [FISH] at $23, 4 leather armor [LARM] at $90, '
+                 'sword [SWOR] at $101.',
+                 '  For Sale: 393 vikings [VIKI] at $62, 78 leaders [LEAD] at '
+                 '$124.',
+                 '  Entertainment available: $649.']
+        
+        for l in lines:
+            region.append_report_description(l)
+        
+        self.assertEqual(region.report, lines)
+    
+    def test_set_weather(self):
+        """Test :meth:`~atlantis.gamedata.region.Region.set_weather`."""
+        region = Region((21, 93, None), 'plain', 'Isshire',
+                        2392, 'vikings', 11016,
+                        {'name': 'Durshire', 'type': 'town'})
+        region.set_weather('clear', 'winter', True, False)
+        
+        self.assertEqual(region.weather,
+                         {'last': 'clear', 'next': 'winter',
+                          'clearskies': True, 'blizzard': False})
+    
+    def test_set_wages(self):
+        """Test :meth:`~atlantis.gamedata.region.Region.set_wages`."""
+        region = Region((21, 93, None), 'plain', 'Isshire',
+                        2392, 'vikings', 11016,
+                        {'name': 'Durshire', 'type': 'town'})
+        region.set_wages(15.6, 3672)
+        
+        self.assertEqual(region.wages, {'productivity': 15.6, 'amount': 3672})
+    
+    def test_set_market(self):
+        """Test :meth:`~atlantis.gamedata.region.Region.set_market`."""
+        region = Region((21, 93, None), 'plain', 'Isshire',
+                        2392, 'vikings', 11016,
+                        {'name': 'Durshire', 'type': 'town'})
+        region.set_market('sell',
+                          [ItemMarket('GRAI', 171, 24, names='grain'),
+                           ItemMarket('LIVE', 109, 24, names='livestock'),
+                           ItemMarket('FISH', 139, 23, names='fish'),
+                           ItemMarket('LARM', 4, 90, names='leather armor'),
+                           ItemMarket('SWOR', 1, 101, name='sword')])
+        
+        self.assertEqual(region.market,
+                         {'sell': [
+                             ItemMarket('GRAI', 171, 24, names='grain'),
+                             ItemMarket('LIVE', 109, 24, names='livestock'),
+                             ItemMarket('FISH', 139, 23, names='fish'),
+                             ItemMarket('LARM', 4, 90, names='leather armor'),
+                             ItemMarket('SWOR', 1, 101, name='sword')]})
+    
+    def test_set_entertainment(self):
+        """Test
+        :meth:`~atlantis.gamedata.region.Region.set_entertainment`."""
+        region = Region((21, 93, None), 'plain', 'Isshire',
+                        2392, 'vikings', 11016,
+                        {'name': 'Durshire', 'type': 'town'})
+        region.set_entertainment(649)
+        
+        self.assertEqual(region.entertainment, 649)
+    
+    def test_set_products(self):
+        """Test :meth:`~atlantis.gamedata.region.Region.set_products`."""
+        region = Region((21, 93, None), 'plain', 'Isshire',
+                        2392, 'vikings', 11016,
+                        {'name': 'Durshire', 'type': 'town'})
+        region.set_products([ItemAmount('LIVE', 66, names='livestock')])
+        
+        self.assertEqual(region.products,
+                         [ItemAmount('LIVE', 66, names='livestock')])
+    
+    def test_set_exit(self):
+        """Test :meth:`~atlantis.gamedasta.region.Region.set_exit`."""
+        region = Region((21, 93, None), 'plain', 'Isshire',
+                        2392, 'vikings', 11016,
+                        {'name': 'Durshire', 'type': 'town'})
+        region.set_exit('north', (21,91, None))
+        
+        self.assertEqual(region.exits, {'north': (21, 91, None)})
+
+    def test_json_methods(self):
+        """Test implementation of 
+        :meth:`~atlantis.helpers.json.JsonSerializeble` interface."""
+        io = StringIO()
+        
+        region = Region((21, 93, None), 'plain', 'Isshire',
+                        2392, 'vikings', 11016,
+                        {'name': 'Durshire', 'type': 'town'})
+        json.dump(region, io, default=Region.json_serialize)
+        io.seek(0)
+        region_new = Region.json_deserialize(json.load(io))
+        self.assertEqual(region, region_new)
+        
+        region.append_report_description('plain (21,93) in Isshire, contains '
+                                         'Durshire [town], 9836 peasants '
+                                         '(vikings), $11016.')
+        region.append_report_description('------------------------------'
+                                         '------------------------------')
+        region.append_report_description('  The weather was clear last month; '
+                                         'it will be clear next month.')
+        region.set_weather('clear', 'clear')
+        region.append_report_description('  Wages: $15.6 (Max: $3672).')
+        region.set_wages(15.6, 3672)
+        region.append_report_description('  Wanted: 171 grain [GRAI] at $24, '
+                                         '109 livestock [LIVE] at $24, 139 '
+                                         'fish [FISH] at $23, 4 leather armor '
+                                         '[LARM] at $90, sword [SWOR] at $101.')
+        region.set_market('sell',
+                          [ItemMarket('GRAI', 171, 24, names='grain'),
+                           ItemMarket('LIVE', 109, 24, names='livestock'),
+                           ItemMarket('FISH', 139, 23, names='fish'),
+                           ItemMarket('LARM', 4, 90, names='leather armor'),
+                           ItemMarket('SWOR', 1, 101, name='sword')])
+        region.append_report_description('  For Sale: 393 vikings [VIKI] at $62,'
+                                         ' 78 leaders [LEAD] at $124.')
+        region.set_market('buy',
+                          [ItemMarket('VIKI', 393, 62, 'vikings'),
+                           ItemMarket('LEAD', 78, 124, 'leaders')])
+        region.append_report_description('  Entertainment available: $649.')
+        region.set_entertainment(649)
+        region.append_report_description('  Products: 66 livestock [LIVE].')
+        region.set_products([ItemAmount('LIVE', 66, names='livestock')])
+        region.append_report_description('')
+        region.append_report_description('Exits:')
+        region.append_report_description("  North : forest (19,91) in Ny'intu.")
+        region.set_exit('north', (19, 91, None))
+        
+        io.seek(0)
+        io.truncate()
+        json.dump(region, io, default=Region.json_serialize)
+        io.seek(0)
+        region_new = Region.json_deserialize(json.load(io))
+        self.assertEqual(region, region_new)
+
+if __name__ == '__main__':
+    unittest.main()
