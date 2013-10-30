d123072ae1df78f74ad360e0db8f4ef8d27eb8c8
David Lozano <david.lozano.ruiz@gmail.com>
2013-10-31 00:35:00 +0100

    Added tests on sequences of RichComparable objects.

--- pyAH/atlantis/helpers/tests/test_comparable.py
+++ pyAH/atlantis/helpers/tests/test_comparable.py
@@ -21,6 +21,10 @@ class TestRichComparable(unittest.TestCase):
         o2 = A(2, [5, 8, 12], 'pedro')
         
         self.assertEqual(o1, o2)
+        
+        l1 = [o1]
+        l2 = [o2]
+        self.assertEqual(l1, l2)
 
 if __name__ == '__main__':
     unittest.main()
