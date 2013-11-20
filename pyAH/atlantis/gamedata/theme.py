546e3b2c4fcf072bb8fd8c33f6976405a5d08d97
David Lozano <david.lozano.ruiz@gmail.com>
2013-11-20 22:46:32 +0100

    New theme module.
    Information about how data is shown (colors, icons, etc) is moved from
    atlantis.gamedata.rules module to atlantis.gamedata.theme.

---
+++ pyAH/atlantis/gamedata/theme.py
@@ -0,0 +1,107 @@
+"""Handles Graphics User Interface theming.
+
+The goal of this package is handle information about how
+game data is to be shown by the Graphics User Interface.
+
+"""
+
+from atlantis.helpers.json import json_load_dict
+from atlantis.helpers.json import JsonSerializable
+from atlantis.helpers.comparable import RichComparable # For testing
+
+import os.path
+
+class Theme(JsonSerializable, RichComparable):
+    """Handles GUI theme.
+    
+    This class holds data about how game data is to be shown by the
+    User Interface.
+    
+    Note that this :class:`Theme` class does nothing about wx (or any
+    other graphics library used) objects, but just read and store
+    theming parameters as colors and bitmap names.
+    
+    """
+    
+    def __init__(self, data_dict=None):
+        """:class:`Theme` constructor.
+        
+        Creates an instance of
+        :class:`Theme` using the data provided, if any. Typically
+        instances of this class will be created via its static methods
+        :meth:`json_deserialize` and :meth:`read_folder`.
+        
+        :param data_dict: dictionary with configuration data. Typically
+            this parameter is used by :meth:`json_deserialize` to
+            create the object.
+        
+        """
+        self._base_folder = None
+        self._data = data_dict
+    
+    def get_art_folder(self):
+        """Return art folder.
+        
+        Art folder is the place where bitmaps for the theme are stored.
+        It's always an **art** folder within the theme folder.
+        
+        :return: a string with the complete art folder.
+        
+        """
+        return os.path.join(self._base_folder, 'art')
+    
+    def json_serialize(self):
+        """Return a serializable version of :class:`Theme`.
+        
+        This method overrides
+        :meth:`~atlantis.helpers.json.JsonSerializable.json_serialize`
+        of :class:`atlantis.helpers.json.JsonSerializable` interface.
+        
+        :return: a *dict* representing the :class:`Theme` object.
+        
+        """
+        return self._data 
+    
+    @staticmethod
+    def json_deserialize(json_object):
+        """Load :class:`Theme` from a deserialized json object.
+        
+        This method overrides
+        :meth:`~atlantis.helpers.json.JsonSerializable.json_deserialize`
+        of :class:`atlantis.helpers.json.JsonSerializable` interface.
+
+        :param json_object: object returned by
+            :func:`atlantis.helpers.json.load`.
+        
+        :return: the :class:`Theme` object from json data.
+        
+        """
+        gc = Theme()
+        gc._data = json_object
+        return gc
+    
+    @staticmethod
+    def read_folder(folder_name):
+        """Load :class:`Theme` from a folder.
+        
+        This method loads data from a rules folder with the following
+        files:
+            
+        - theme.json
+            Colors and bitmaps used by the GUI to show data.
+
+        :param folder_name: folder where the files are read from.
+        
+        :return: a :class:`Theme` object.
+        
+        """
+        gd = Theme()
+        
+        with open(os.path.join(folder_name, 'theme.json')) as f:
+            gd._data = json_load_dict(f)
+            gd._base_folder = folder_name
+        
+        return gd
+        
+if __name__ == '__main__':
+    pass
---
+++ pyAH/atlantis/gamedata/theme.rst
@@ -0,0 +1,20 @@
+------------------------------
+:mod:`atlantis.gamedata.theme`
+------------------------------
+
+.. automodule:: atlantis.gamedata.theme
+   
+Public classes in :mod:`atlantis.gamedata.theme` module:
+
+.. autosummary::
+   :nosignatures:
+   
+   Theme
+ 
+:class:`~atlantis.gamedata.theme.Theme`
++++++++++++++++++++++++++++++++++++++++
+
+.. autoclass:: atlantis.gamedata.theme.Theme
+   :members:
+   :special-members: __init__
+   :member-order: bysource
