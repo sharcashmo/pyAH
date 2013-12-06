bfd9e109f2966367a4bc28e14e7796dd138ce2ee
David Lozano <david.lozano.ruiz@gmail.com>
2013-12-06 21:18:14 +0100

    New module to manage graphic resources, as cursors.

---
+++ pyAH/atlantis/wxgui/resources.py
@@ -0,0 +1,11 @@
+"""Holds common resources for :ref:`atlantis.wxgui` package modules."""
+
+import os
+import wx
+
+_resources_folder, dummy = os.path.split(__file__)
+_resources_folder = os.path.join(_resources_folder, 'resources')
+
+def get_drag_cursor():
+    """Return a :class:`wx.Cursor` to be used for dragging."""
+    return wx.Cursor(wx.Image(os.path.join(_resources_folder, 'drag_cursor.png')))
